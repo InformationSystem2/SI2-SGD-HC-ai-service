28854653dea9218b6034293923430f8e80905123
SCANDAL9 <duglasrivero2@gmail.com>
2026-06-03 14:40:56 -0400

    config prod CORS

--- app/main.py
+++ app/main.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+import os
+from pathlib import Path
+
+current_file_dir = Path(__file__).resolve().parent
+load_dotenv(dotenv_path=current_file_dir.parent / ".env")
+load_dotenv(dotenv_path=current_file_dir.parent.parent / "sgd_spring-boot" / ".env")
+
+CORS_ORIGIN = os.getenv("CORS_ALLOWED_ORIGINS", "*")
+
 from app.routers import ocr, reports, analytics
 
 app = FastAPI(title="SGD-HC OCR Service & Reports", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # en prod cambiás esto al dominio del backend Spring
+    allow_origins=[CORS_ORIGIN],
     allow_methods=["*"],
     allow_headers=["*"],
 )
